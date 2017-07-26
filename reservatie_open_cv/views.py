0a63746028acf8a8565f233cb43b4de2e6d42000
Jefwillems <jefwillems100@gmail.com>
2017-07-26 16:00:16 +0200

    new data.html

--- reservatie_open_cv/views.py
+++ reservatie_open_cv/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 def index(request):
     random_tekst = "hallo ik ben Jef"
     return render(request, 'reservatie_open_cv/index.html', {'test': random_tekst})
+
+
+def data(request):
+    return render(request, 'reservatie_open_cv/data.html', {'var_test': "var_test"})
