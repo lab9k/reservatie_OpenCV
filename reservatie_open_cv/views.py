f347d5a2735004aac96910ec920cf1c07d06edd3
Jefwillems <jefwillems100@gmail.com>
2017-07-26 14:47:58 +0200

    index view

--- reservatie_open_cv/views.py
+++ reservatie_open_cv/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import random
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Hello world!</h1>')
+    random_tekst = "hallo ik ben Jef"
+    return render(request, 'reservatie_open_cv/index.html', {'test': random_tekst})
