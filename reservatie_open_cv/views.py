405505f1c1365b08f0ebddb1d56d733aeac77eb3
Jefwillems <jefwillems100@gmail.com>
2017-07-26 17:31:52 +0200

    get/post

--- reservatie_open_cv/views.py
+++ reservatie_open_cv/views.py
@@ -4,18 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    random_tekst = "hallo ik ben Jef"
-    return render(request, 'reservatie_open_cv/index.html', {'test': random_tekst})
+    return render(request, 'reservatie_open_cv/index.html')
 
 
 def data(request):
     if request.method == "GET":
-        return render(request, 'reservatie_open_cv/data.html', {'var_test': "var_test"})
+        return render(request, 'reservatie_open_cv/data.html')
     if request.method == "POST":
         print(request.POST)
         # TODO do something
         pass
-    return render(request, 'reservatie_open_cv/data.html', {'var_test': "var_test"})
+    return render(request, 'reservatie_open_cv/data.html')
 
 
 def loading(request):
