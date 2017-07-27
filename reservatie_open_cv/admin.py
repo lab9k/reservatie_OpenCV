c2fa69ccc233928b8a64f461eb0705d04c607241
Jefwillems <jefwillems100@gmail.com>
2017-07-27 11:28:04 +0200

    added models for making reservations

--- reservatie_open_cv/admin.py
+++ reservatie_open_cv/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Zaal
+
 
 # Register your models here.
+class ZaalAdmin(admin.ModelAdmin):
+    fields = ('naam', 'locatie', 'aantal_plaatsen')
+    search_fields = ('naam', 'locatie', 'aantal_plaatsen')
+    list_display = ('naam', 'locatie', 'aantal_plaatsen')
+
+
+admin.site.register(Zaal, ZaalAdmin)
--- reservatie_open_cv/models.py
+++ reservatie_open_cv/models.py
@@ -1,4 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
+
 
 # Create your models here.
 
+class Zaal(models.Model):
+    naam = models.CharField(max_length=100, blank=False)
+    locatie = models.CharField(max_length=100, blank=False)
+    aantal_plaatsen = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.locatie + " : " + self.naam
+
+
+class Reservatie(models.Model):
+    user = models.ForeignKey(User)
+    date = models.DateTimeField(null=False, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    for_zaal = models.OneToOneField(
+        Zaal,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return "Reservatie: " + str(self.date) + " door user: " + str(self.user)
