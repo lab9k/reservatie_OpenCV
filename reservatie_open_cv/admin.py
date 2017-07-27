2a1a9d718de89ca104cf52a99b426931b4870c83
Jefwillems <jefwillems100@gmail.com>
2017-07-27 11:41:17 +0200

    documentation

--- reservatie_open_cv/admin.py
+++ reservatie_open_cv/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from .models import Zaal
 
 
-# Register your models here.
 class ZaalAdmin(admin.ModelAdmin):
+    """
+    Configuration to display 'Zaal' models in the Django admin site.
+    """
+    # Fields to be used in the form for creating 'Zaal' objects
     fields = ('naam', 'locatie', 'aantal_plaatsen')
+    # Fields to lookup when using the search bar in Django admin
     search_fields = ('naam', 'locatie', 'aantal_plaatsen')
+    # Fields to be displayed in the Django admin overview for 'Zaal'
     list_display = ('naam', 'locatie', 'aantal_plaatsen')
 
 
+# registers Zaal andhis configuration ZaalAdmin in the Django-admin site
 admin.site.register(Zaal, ZaalAdmin)
