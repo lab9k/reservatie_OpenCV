623ddd7752855897eb86b54799667b8aba30328a
JorgWieme <jorg.wieme@ugent.be>
2017-07-27 14:26:11 +0200

    Update recognize.py

--- face_Recognition/recognize.py
+++ face_Recognition/recognize.py
@@ -18,9 +18,9 @@ def recon(tuple):
         id_pers, conf = recognizer.predict(image[y:y + h, x:x + w])
         # zie trainner
         if id_pers == 1:
-            id_pers = "Person 1"
+            id_pers = "Jorg"
         elif id_pers == 2:
-            id_pers = "Person 2"
+            id_pers = "Jana"
         else:
             id_pers = "Unknown"
         print id_pers
