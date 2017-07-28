1abe4ec0e8dcb86739787f5d3fecf63162b3a525
JorgWieme <jorg.wieme@ugent.be>
2017-07-28 13:56:03 +0200

    Update recognize.py

--- reservatie_open_cv/recognize.py
+++ reservatie_open_cv/recognize.py
@@ -1,36 +1,29 @@
 # import cv2
-# import os
-# from lab9k.settings import PROJECT_ROOT
-#
-#
-# def recon(now, mtuple):
+# import sqlite3
+# 
+# 
+# def recon(tuple):
 #     # krijgen map locatie boolean en tupel met x y w h
-#
-#     x, y, h, w = mtuple
-#     x = int(x)
-#     y = int(y)
-#     h = int(h)
-#     w = int(w)
+#     now, bool, tupel = tuple
+#     x, y, w, h = tupel
 #     # anders zou het crashen
 #     if bool:
 #         # maakt de facerecon aan
 #         recognizer = cv2.createLBPHFaceRecognizer()
 #         # de trainer file word in geladen
-#         print (os.path.join(PROJECT_ROOT, 'trainer.yml'))
-#         recognizer.load(os.path.join(PROJECT_ROOT, 'trainer.yml'))
+#         recognizer.load('trainerAlt2Alt2.yml')
 #         # foto ingeladen
-#
-#         image = cv2.imread(os.path.join(PROJECT_ROOT, "IMAGES/{}/gray320.jpg".format(now)), 0)
+#         image = cv2.imread("IMAGES/{}/gray320.jpg".format(now), 0)
 #         # laat het algoritme toe op de foto en dit return een id(uit de
 #         # trainner)
-#         print(image)
-#         print(x, y)
 #         id_pers, conf = recognizer.predict(image[y:y + h, x:x + w])
-#         # zie trainner
-#         if id_pers == 1:
-#             id_pers = "Jorg"
-#         elif id_pers == 2:
-#             id_pers = "Jana"
+#         print(conf)
+#         # aanpassen als er een database bij komt
+#         if conf > 50 and conf < 120:
+#                 conn = sqlite3.connect("db.sqlite3")
+#                 c = conn.cursor()
+#                 for per in c.execute('SELECT * FROM auth_user WHERE id=?', str(id_pref)):
+#                         name = per
 #         else:
-#             id_pers = "Unknown"
-#         return id_pers
+#                 name = "Unknown"
+#         return name
