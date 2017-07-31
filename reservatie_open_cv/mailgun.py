e7c53767882180c519abec9021611533461f9ce8
Jefwillems <jefwillems100@gmail.com>
2017-07-31 15:33:02 +0200

    documentation mailgun

--- reservatie_open_cv/mailgun.py
+++ reservatie_open_cv/mailgun.py
@@ -3,6 +3,14 @@ from lab9k.settings import MAILGUN_API_KEY
 
 
 def send_async_message(from_email, to_email, subject, text):
+    """
+    Sends an email with the mailgun api (<a href="https://www.mailgun.com/">mailgun</a> )
+    :param from_email: mailgun email address the mail will be sent from
+    :param to_email: email address the email will be sent to
+    :param subject: subject of the email
+    :param text: text of the email
+    :return: requests.Reponse
+    """
     return post(
         "https://api.mailgun.net/v3/mail.lab9k.gent/messages",
         auth=("api", MAILGUN_API_KEY),
