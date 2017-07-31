c0cd512d31ecc10c15f32252be3a1472878115c0
JorgWieme <jorg.wieme@ugent.be>
2017-07-31 15:34:10 +0200

    change a bit

--- reservatie_open_cv/recognize.py
+++ reservatie_open_cv/recognize.py
@@ -4,26 +4,28 @@ from lab9k.settings import PROJECT_ROOT
 
 
 def recon(now, mtuple):
-    # krijgen map locatie boolean en tupel met x y w h
-
+    # we receive a time(string) and a tuple with 4 coordinates, cast everything
+    # to the correct type
     x, y, h, w = mtuple
     x = int(x)
     y = int(y)
     h = int(h)
     w = int(w)
-    # maakt de facerecognizer aan
+    # create a recognizer, we opted for a LBPHFaceRecognizer cause trying this
+    # is easyer then Eigenfaces and Fisherfaces
     recognizer = cv2.createLBPHFaceRecognizer()
-    # de trainer file word in geladen, elke keer als je een nieuw persoon wilt
-    # toevoegen moet je de volledige file opnieuw maken
-    # door het script uit te voeren (/face_Recognition/build_trainer.py)
+    # we load the trainer file, if you want to add a person to your trainer
+    # in LBPH, you will need a script to rebuild the trainer file
+    # script located at /face_Recognition/build_trainer.py
+    # Name of trainer.yml is Alt2Alt2 cause the HAAR cascade I used are
+    # .../haarcascade_frontalface_alt2.xml
     recognizer.load(os.path.join(PROJECT_ROOT, 'trainerAlt2Alt2.yml'))
-    # foto ingeladen
+    # load image in var
     image = cv2.imread(
         os.path.join(PROJECT_ROOT, "IMAGES/{}/gray320.jpg".format(now)), 0)
+    # try to predict who is on the picture
     id_pers, conf = recognizer.predict(image[y:y + h, x:x + w])
-    # geeft het id door zal worden opgehaald in database
-    # conf word gebruikt voor meerdere zekerheid van de foto,
-    # beter trainen betere waarden
+    # id for database and conf for % matching, more training = better detecting
     if not 50 < conf < 120:
         id_pers = "Unknown"
     return id_pers
